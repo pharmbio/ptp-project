39b5af2f36b9b548a8ccbd4d456de5d432f64e72
Samuel Lampa <samuel.lampa@gmail.com>
2017-06-21 09:29:20 +0200

    Add go-code

---
+++ exp/20170517-exploredb/explore_excapedb.go
@@ -0,0 +1,44 @@
+// Workflow written in SciPipe.
+// For more information about SciPipe, see: http://scipipe.org
+package main
+
+import (
+	"fmt"
+	sp "github.com/scipipe/scipipe"
+)
+
+func main() {
+	// --------------------------------
+	// Create a pipeline runner
+	// --------------------------------
+
+	run := sp.NewPipelineRunner()
+
+	// --------------------------------
+	// Initialize processes and add to runner
+	// --------------------------------
+
+	excapeDBFileName := "pubchem.chembl.dataset4publication_inchi_smiles.tsv.xz"
+	dlExcapeDB := sp.NewFromShell("dlDB", fmt.Sprintf("wget https://zenodo.org/record/173258/files/%s -O {o:excapexz}", excapeDBFileName))
+	dlExcapeDB.SetPathStatic("excapexz", excapeDBFileName)
+	run.AddProcess(dlExcapeDB)
+
+	unPackDB := sp.NewFromShell("unPackDB", "xzcat {i:xzfile} > {o:unxzed}")
+	unPackDB.SetPathReplace("xzfile", "unxzed", ".xz", "")
+	run.AddProcess(unPackDB)
+
+	sink := sp.NewSink()
+	run.AddProcess(sink)
+
+	// --------------------------------
+	// Connect workflow dependency network
+	// --------------------------------
+	unPackDB.In["xzfile"].Connect(dlExcapeDB.Out["excapexz"])
+	sink.Connect(unPackDB.Out["unxzed"])
+
+	// --------------------------------
+	// Run the pipeline!
+	// --------------------------------
+
+	run.Run()
+}
