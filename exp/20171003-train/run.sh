0f527b5df7cded4a55fb652d25c0340ef63bcb5e
Samuel Lampa <samuel.lampa@gmail.com>
2017-10-16 21:33:14 +0200

    Add mail config in slurm script

---
+++ exp/20171003-train/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -l
+#SBATCH -A snic2017-7-89
+#SBATCH -p node
+#SBATCH -N 1
+#SBATCH -J Run_PTP_WF_in_SciPipe
+#SBATCH -t 7-00:00:00
+#SBATCH --mail-user samuel.lampa@farmbio.uu.se
+#SBATCH --mail-type FAIL,END
+module load java/sun_jdk1.8.0_92
+go run train_models.go -maxcores 20 -geneset bowes44
