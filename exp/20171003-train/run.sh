06e8b9c430b7daea88f99f2e6f384bfef4fad20b
Samuel Lampa <samuel.lampa@gmail.com>
2017-10-17 13:06:40 +0200

    Don't set -n in SBATCH, and don't run via srun, so as not to lock to few cores

--- exp/20171003-train/run.sh
+++ exp/20171003-train/run.sh
@@ -2,10 +2,9 @@
 #SBATCH -A snic2017-7-89
 #SBATCH -p node
 #SBATCH -N 1
-#SBATCH -n 1
 #SBATCH -J Run_PTPWF_on_one_node
-#SBATCH -t 1-00:00:00
+#SBATCH -t 4-00:00:00
 #SBATCH --mail-user samuel.lampa@farmbio.uu.se
 #SBATCH --mail-type FAIL,END
 module load java/sun_jdk1.8.0_92
-srun -n 1 go run train_models.go -threads 2 -maxtasks 10 -geneset bowes44
+go run train_models.go -threads 2 -maxtasks 10 -geneset bowes44
