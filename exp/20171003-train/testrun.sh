481f84d8738840ebdbc3283e61aa3a3b55eaef69
Samuel Lampa <samuel.lampa@gmail.com>
2017-10-18 16:37:21 +0200

    Add testrun.sh script

---
+++ exp/20171003-train/testrun.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -l
+#SBATCH -A snic2017-7-89
+#SBATCH -p devel
+#SBATCH -N 1
+#SBATCH -J Test_PTPWF_on_one_node
+#SBATCH -t 1:00:00
+#SBATCH --mail-user samuel.lampa@farmbio.uu.se
+#SBATCH --mail-type FAIL,END
+module load java/sun_jdk1.8.0_92
+go run train_models.go -threads 2 -maxtasks 10 -geneset smallest4
