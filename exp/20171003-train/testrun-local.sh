b0a8d4f9b906f22fe3fcfccffc625850ac3a7b7d
Samuel Lampa <samuel.lampa@gmail.com>
2017-10-20 18:16:04 +0200

    Add testrun-local.sh script

---
+++ exp/20171003-train/testrun-local.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -l
+#SBATCH -A snic2017-7-89
+#SBATCH -p devel
+#SBATCH -N 1
+#SBATCH -J Test_PTPWF_on_one_node
+#SBATCH -t 1:00:00
+#SBATCH --mail-user samuel.lampa@farmbio.uu.se
+#SBATCH --mail-type FAIL,END
+go run train_models.go -threads 4 -maxtasks 4 -geneset smallestgene
