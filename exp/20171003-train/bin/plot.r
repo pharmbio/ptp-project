52bb757e1bcf89c0fe3da643b70c20a227bf2021
Samuel Lampa <samuel.lampa@gmail.com>
2017-10-23 16:23:37 +0200

    Add plot.r script

---
+++ exp/20171003-train/bin/plot.r
@@ -0,0 +1,35 @@
+#!/usr/bin/Rscript
+
+# ------------------------------------------------------------------------
+# Commandline parsing
+# ------------------------------------------------------------------------
+library(getopt)
+optspec = matrix(c(
+  'infile', 'i', 1, 'character',
+  'outfile', 'o', 1, 'character',
+  'format', 'f', 1, 'character',
+), byrow=TRUE, ncol=4);
+opt = getopt(optspec);
+
+# if help was asked for print a friendly message
+# and exit with a non-zero error code
+if ( is.null(opt$format) || is.null(opt$infile) || is.null(opt$outfile) ) {
+  cat('Usage: Rscript plot_heatmap.r -i infile -o outfile -f (png|pdf)\n');
+  q(status=1);
+}
+
+# ------------------------------------------------------------------------
+# Create and plot the heatmap
+# ------------------------------------------------------------------------
+
+# Set output format
+if (opt$format == 'png') {
+	png(opt$outfile);
+} else if (opt$format =='pdf') {
+	pdf(opt$outfile);
+}
+
+d <- read.csv(opt$infile, sep = '\t', header = TRUE)
+plot(d)
+
+dev.off()
