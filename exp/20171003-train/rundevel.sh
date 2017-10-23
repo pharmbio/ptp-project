1f4ca6a97c42e23f1ff249820163987bd053598e
Samuel Lampa <samuel.lampa@gmail.com>
2017-10-23 17:24:01 +0200

    Add rundevel.sh script

---
+++ exp/20171003-train/rundevel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+#SBATCH -A snic2017-7-89
+#SBATCH -p devel
+#SBATCH -N 1
+#SBATCH -J Run_PTPWF_on_one_node
+#SBATCH -t 1:00:00
+#SBATCH --mail-user samuel.lampa@farmbio.uu.se
+#SBATCH --mail-type BEGIN,FAIL,END
+module load java/sun_jdk1.8.0_92
+module load R/3.4.0
+go run train_models.go components.go -threads 2 -maxtasks 10 -geneset bowes44
