0a6ace7b75768fcfac595c590c354da813fbc075
Samuel Lampa <samuel.lampa@gmail.com>
2017-10-24 13:55:42 +0200

    Create merged plot instead of multiple ones

--- exp/20171003-train/bin/plot_summary.r
+++ exp/20171003-train/bin/plot_summary.r
@@ -33,20 +33,61 @@ d <- read.csv(opt$infile, sep = '\t', header = TRUE);
 # --------------------------------------------------------------------------------
 # Read in file manually for debugging:
 # --------------------------------------------------------------------------------
-# d <- read.csv("../dat/final_models/summary.sorted.tsv", sep = '\t', header = TRUE);
+# dev.off()
+# setwd(dir = "/media/samuel/SAMUELLAMPA/proj/ptp/exp/20171003-train/")
+# d <- read.csv("dat/final_models/summary.sorted.tsv", sep = '\t', header = TRUE);
 # --------------------------------------------------------------------------------
 
-par(mfrow=c(4,1));
-
 counts <- rbind(d$Active, d$Nonactive)
-barplot(counts, names=d$Gene, beside = FALSE, col=c("white", "black"), main = 'Active / Nonactive compounds count (log-scaled)', log="y", las=2, cex.axis=0.8);
+  
+# Force to avoid scientific numerical format (sci-penalty)
+options(scipen=1, digits="0");
 
-barplot(d$Efficiency,names=d$Gene, ylim=c(0,1), main = 'Efficiency', las=2);
+# Set margins (in inches, thus mai), for the whole plot
+par(mai=c(1.2,1.2,1.2,1.2))
+# Plot active/nonactive compounds
+bp <- barplot(counts, 
+        names=d$Gene, 
+        beside = FALSE, 
+        col=c("white", "#dddddd"), 
+        main = 'Active / Nonactive compounds count (log-scaled)', 
+        log="y", 
+        las=2,
+        cex.names=0.8,
+        ylim=c(1,1e8),
+        legend = FALSE,
+        xlab=NA)
+mtext("Compounds", side=2, line=3.6);
+legend("bottomright", 
+       c("Active", "Nonactive"),
+       fill=c("white", "#dddddd", NA, NA),
+);
 
-barplot(d$Validity,names=d$Gene, ylim=c(0,1), main = 'Validity', las=2);
+par(new=TRUE);
+      
+# Plot training time (minutes)
+plot(bp,d$ExecTimeMS/(1000*60), type="b", col="red", axes=FALSE, log="y", ylab=NA, xlab=NA);
+axis(2, las=2, col.axis="red", col.ticks="red");
+mtext("Training time (m)", side=2, line=5, col="red")
+par(new=TRUE)
 
-barplot(d$ModelFileSize/(1024*1024),names=d$Gene, main = 'Model file size (MB)', las=2);
+# Plot 1-Efficiency
+plot(bp,1-d$Efficiency, type="b", axes=FALSE, col="blue", col.axis="blue", las=2, ylab=NA, xlab=NA, ylim=c(0,1));
+axis(4, las=2, col="blue", col.axis="blue")
+mtext("1-Efficiency", side=4, line=4, col="blue")
+
+# --------------------------------------------------------------------------------
+# Alternative legend, with the line plots included:
+# --------------------------------------------------------------------------------
+#legend("bottomright", 
+#       c("Active", "Nonactive", "Training time (m)", "1-Efficiency"), 
+#       pch=c(NA, NA, 1, 1), 
+#       col=c(NA, NA, "red", "blue"), 
+#       fill=c("white", "#dddddd", NA, NA),
+#       border=c("black", "black", NA, NA),
+#   );
+# --------------------------------------------------------------------------------
 
 dev.off()
-
+# Avoid sending non-zero exit values on exit
 quit(save = "no", status = 0, runLast = FALSE)
