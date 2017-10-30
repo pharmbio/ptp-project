cd91df081aff95630eccbf6dbc675bb1a87e8929
Samuel Lampa <samuel.lampa@gmail.com>
2017-10-30 15:01:08 +0100

    Use fat node, and all cores

--- exp/20171003-train/run.sh
+++ exp/20171003-train/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -l
 #SBATCH -A snic2017-7-89
 #SBATCH -p node
+#SBATCH -C mem256GB
 #SBATCH -N 1
 #SBATCH -J Run_PTPWF_on_one_node
 #SBATCH -t 4-00:00:00
 #SBATCH --mail-user samuel.lampa@farmbio.uu.se
 #SBATCH --mail-type BEGIN,FAIL,END
 module load java/sun_jdk1.8.0_92
-go run train_models.go components.go -threads 2 -maxtasks 10 -geneset bowes44min100percls
+go run train_models.go components.go -threads 2 -maxtasks 20 -geneset bowes44min100percls
