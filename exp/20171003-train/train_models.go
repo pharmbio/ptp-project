a4dbddc1edaf895ed1190e6fe90c0c7e582b69f7
Samuel Lampa <samuel.lampa@gmail.com>
2017-11-03 15:17:55 +0100

    Fix tripped up sorting because of shifted columns

--- exp/20171003-train/train_models.go
+++ exp/20171003-train/train_models.go
@@ -283,7 +283,7 @@ func main() {
 		finalModelsSummary.InTargetDataCount.Connect(countTargetDataRows.Out("count"))
 	}
 
-	sortSummaryOnDataSize := wf.NewProc("sort_summary", "sort -n -k 9 {i:summary} > {o:sorted}")
+	sortSummaryOnDataSize := wf.NewProc("sort_summary", "sort -n -k 10 {i:summary} > {o:sorted}")
 	sortSummaryOnDataSize.SetPathReplace("summary", "sorted", ".tsv", ".sorted.tsv")
 	sortSummaryOnDataSize.In("summary").Connect(finalModelsSummary.OutSummary)
 
