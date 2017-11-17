4828f765646926dba9c111dc00a0077238c7f63f
Samuel Lampa <samuel.lampa@gmail.com>
2017-11-17 16:59:18 +0100

    New name of ptp workflow, and run for 7 days

--- exp/20171003-train/run.sh
+++ exp/20171003-train/run.sh
@@ -3,8 +3,8 @@
 #SBATCH -p node
 #SBATCH -C mem256GB
 #SBATCH -N 1
-#SBATCH -J Run_PTPWF_on_one_node
-#SBATCH -t 4-00:00:00
+#SBATCH -J ptp_workflow
+#SBATCH -t 7-00:00:00
 #SBATCH --mail-user samuel.lampa@farmbio.uu.se
 #SBATCH --mail-type BEGIN,FAIL,END
 module load java/sun_jdk1.8.0_92
