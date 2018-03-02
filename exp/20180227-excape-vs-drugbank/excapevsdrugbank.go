c11e254fede66b378ca8c8167331d0690aa67740
Samuel Lampa <samuel.lampa@gmail.com>
2018-03-02 19:34:18 +0100

    Implement an XML parsing component with scipipe.Task.CustomExecute

--- exp/20180227-excape-vs-drugbank/excapevsdrugbank.go
+++ exp/20180227-excape-vs-drugbank/excapevsdrugbank.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"encoding/xml"
+	"fmt"
+	"os"
+
 	sp "github.com/scipipe/scipipe"
 )
 
 func main() {
 	wf := sp.NewWorkflow("exvsdb", 1)
-	wf.NewProc("dl", "curl -Lfv -o filename.zip -u ... https://www.drugbank.ca/releases/5-0-11/downloads/all-full-database")
+	//wf.NewProc("dl", "curl -Lfv -o filename.zip -u ... https://www.drugbank.ca/releases/5-0-11/downloads/all-full-database")
+
+	drugBank := sp.NewFileIPGenerator(wf, "drugbank_file", "dat/drugbank.xml")
+
+	xmlParser := wf.NewProc("xmlparser", "# {i:xml}")
+	xmlParser.In("xml").Connect(drugBank.Out())
+	xmlParser.CustomExecute = func(t *sp.Task) {
+		fh, err := os.Open(t.InPath("xml"))
+		if err != nil {
+			sp.Fail("Could not open file", t.InPath("xml"))
+		}
+		xmlDec := xml.NewDecoder(fh)
+
+		drugBank := &Drugbank{}
+		decErr := xmlDec.Decode(drugBank)
+		if decErr != nil {
+			sp.Fail("Could not decode XML!")
+		}
+		for _, drug := range drugBank.Drugs {
+			fmt.Println("Drug: ", drug.Name)
+		}
+	}
 
 	//apprZip := wf.NewProc("download_approved", "curl -Lf -o {o:zipfile} -u ... https://www.drugbank.ca/releases/5-0-11/downloads/approved-drug-sequences")
 	//apprZip.SetPathStatic("zipfile", "drug_sequences_approved.zip")
@@ -22,4 +47,11 @@ func main() {
 }
 
 type Drugbank struct {
+	XMLName xml.Name `xml:"drugbank"`
+	Drugs   []Drug   `xml:"drug"`
+}
+
+type Drug struct {
+	XMLName xml.Name `xml:"drug"`
+	Name    string   `xml:"name"`
 }
