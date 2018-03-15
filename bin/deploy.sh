53e20239b643a8207149f60c8c719919e77b932e
Samuel Lampa <samuel.lampa@gmail.com>
2018-03-15 11:47:18 +0100

    Enable sending user and pass as arguments to deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 MODELFILE=$1
 CATEGORY=$2
+USERNAME=$3
+PASSWORD=$4
 if [[ -z $MODELFILE || -z $CATEGORY ]]; then
-    echo "Usage: deploy.sh <modelfile.jar> <category>";
+    echo "Usage: deploy.sh <modelfile.jar> <category> [username [password]]";
     exit 1
 fi;
 
 KEYCLOAK_URL=http://keycloak-test2-modweb.130.238.55.60.nip.io/auth/
 
-echo "User name:"
-read USERNAME
-echo "Password:"
-read PASSWORD
+if [[ -z $USERNAME ]]; then
+    echo "User name:"
+    read USERNAME
+fi;
+if [[ -z $PASSWORD ]]; then
+    echo "Password:"
+    read PASSWORD
+fi;
 MW_URL=http://modelingweb-test4-modweb.130.238.55.60.nip.io/
 RESULT=`curl --data "grant_type=password&client_id=modelingweb&username=$USERNAME&password=$PASSWORD" ${KEYCLOAK_URL}realms/toxhq/protocol/openid-connect/token`
 TOKEN=`echo $RESULT | jq -r '.access_token'`
