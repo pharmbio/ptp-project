dd1dbb688c0f7e64fc83923958dc77f497a2ff7e
Samuel Lampa <samuel.lampa@gmail.com>
2018-03-16 14:30:23 +0100

    Implement cross-check between ExcapeDB and DrugBank

--- exp/20180227-excape-vs-drugbank/excapevsdrugbank.go
+++ exp/20180227-excape-vs-drugbank/excapevsdrugbank.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"encoding/xml"
+	"io"
 	"os"
 
 	sp "github.com/scipipe/scipipe"
@@ -38,9 +40,9 @@ func main() {
 	// Compound IDs
 	drugBankCompIDCmd := "csvcut -K 1 -c 11,14 {i:drugbankcsv} | sed '/^,$/d' | sort -V > {o:compids}"
 
-	drugBankCompIDsAppr := wf.NewProc("drugbank_compids_appr", drugBankCompIDCmd)
-	drugBankCompIDsAppr.SetPathExtend("drugbankcsv", "compids", ".compids.csv")
-	drugBankCompIDsAppr.In("drugbankcsv").Connect(unzipApproved.Out("csv"))
+	drugBankCompIDsApprov := wf.NewProc("drugbank_compids_appr", drugBankCompIDCmd)
+	drugBankCompIDsApprov.SetPathExtend("drugbankcsv", "compids", ".compids.csv")
+	drugBankCompIDsApprov.In("drugbankcsv").Connect(unzipApproved.Out("csv"))
 
 	drugBankCompIDsWithdr := wf.NewProc("drugbank_compids_withdr", drugBankCompIDCmd)
 	drugBankCompIDsWithdr.SetPathExtend("drugbankcsv", "compids", ".compids.csv")
@@ -48,7 +50,7 @@ func main() {
 
 	mergeApprWithdr := wf.NewProc("merge_appr_withdr", "cat {i:in1} {i:in2} | sort -V | uniq > {o:out}")
 	mergeApprWithdr.SetPathStatic("out", "dat/drugbank_compids_all.csv")
-	mergeApprWithdr.In("in1").Connect(drugBankCompIDsAppr.Out("compids"))
+	mergeApprWithdr.In("in1").Connect(drugBankCompIDsApprov.Out("compids"))
 	mergeApprWithdr.In("in2").Connect(drugBankCompIDsWithdr.Out("compids"))
 
 	// ExcapeDB
@@ -71,6 +73,73 @@ func main() {
 	sortTsv.SetPathExtend("unsorted", "sorted", ".sorted.tsv")
 	sortTsv.In("unsorted").Connect(xmlToTSV.Out("tsv"))
 
+	excapeDBVsDrugBank := wf.NewProc("exc_vs_drb", "# Custom Go function with inputs: {i:excapedb_ids}, {i:approv_ids}, {i:withdr_ids} and output: {o:stats}")
+	excapeDBVsDrugBank.SetPathStatic("stats", "dat/excapedb_vs_drugbank_stats.json")
+	excapeDBVsDrugBank.In("excapedb_ids").Connect(extractOrigEntryID.Out(""))
+	excapeDBVsDrugBank.In("approv_ids").Connect(drugBankCompIDsApprov.Out(""))
+	excapeDBVsDrugBank.In("withdr_ids").Connect(drugBankCompIDsWithdr.Out(""))
+	excapeDBVsDrugBank.CustomExecute = func(t *sp.Task) {
+		approvCnt := 0
+		withdrCnt := 0
+
+		approvIds := map[string]bool{}
+		approvFile := t.InIP("approv_ids").Open()
+		approvCsvReader := csv.NewReader(approvFile)
+		for {
+			rec, err := approvCsvReader.Read()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				sp.Fail(err)
+			}
+			approvIds[rec[0]] = true
+			approvIds[rec[1]] = true
+		}
+		approvFile.Close()
+
+		withdrIds := map[string]bool{}
+		withdrFile := t.InIP("withdr_ids").Open()
+		withdrCsvReader := csv.NewReader(withdrFile)
+		for {
+			rec, err := withdrCsvReader.Read()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				sp.Fail(err)
+			}
+			withdrIds[rec[0]] = true
+			withdrIds[rec[1]] = true
+		}
+		withdrFile.Close()
+
+		excIdsFile := t.InIP("excapedb_ids").Open()
+		excIdsCsvReader := csv.NewReader(excIdsFile)
+		for {
+			rec, err := excIdsCsvReader.Read()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				sp.Fail(err)
+			}
+			if _, ok := approvIds[rec[0]]; ok {
+				approvCnt++
+			}
+			if _, ok := withdrIds[rec[0]]; ok {
+				withdrCnt++
+			}
+		}
+		excIdsFile.Close()
+		totStats := map[string]int{}
+		totStats["excapedb_compounds_in_drugbank_approved"] = approvCnt
+		totStats["excapedb_compounds_in_drugbank_withdrawn"] = withdrCnt
+		totStats["excapedb_compounds_in_drugbank_total"] = approvCnt + withdrCnt
+		statsJSON, err := json.Marshal(totStats)
+		if err != nil {
+			sp.Fail(err)
+		}
+		t.OutIP("stats").Write(statsJSON)
+	}
+
 	wf.Run()
 }
 
@@ -83,7 +152,7 @@ func NewXMLToTSVFunc() func(t *sp.Task) {
 			sp.Fail("Could not open file", t.InPath("xml"))
 		}
 
-		tsvWrt := csv.NewWriter(t.OutTargets["tsv"].OpenWriteTemp())
+		tsvWrt := csv.NewWriter(t.OutIP("tsv").OpenWriteTemp())
 		tsvWrt.Comma = '\t'
 		tsvHeader := []string{"inchikey", "status", "chembl_id", "pubchem_sid", "pubchem_cid"}
 		tsvWrt.Write(tsvHeader)
