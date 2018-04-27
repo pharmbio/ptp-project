b59afd80864a8b395f69e69f4321c1b694caec14
Samuel Lampa <samuel.lampa@gmail.com>
2018-04-27 15:25:03 +0200

    Fix bug in upload script

--- exp/20171123-fillup/upload_final_models.sh
+++ exp/20171123-fillup/upload_final_models.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
+set +x
+
 KEYCLOAK_URL=http://keycloak-test2-modweb.130.238.55.60.nip.io/auth
 read -p "Username: " USERNAME
 read -p "Password: " PASSWORD
 echo "$USERNAME:$PASSWORD"
 MW_URL=http://modelingweb-test4-modweb.130.238.55.60.nip.io
 
-RESULT=$(curl --data "grant_type=password&client_id=modelingweb&username=$USERNAME&password=$PASSWORD" ${KEYCLOAK_URL}realms/toxhq/protocol/openid-connect/token)
-echo "Keycloak result: \n"$RESULT
+KC_COMMAND="curl --data grant_type=password&client_id=modelingweb&username=$USERNAME&password=$PASSWORD ${KEYCLOAK_URL}/realms/toxhq/protocol/openid-connect/token"
+echo "Executing Keycloak command: $KC_COMMAND";
+RESULT=$($KC_COMMAND)
+echo "Keycloak result: "$RESULT
 TOKEN=$(echo $RESULT | jq -r '.access_token')
+echo "Keycloak token: "$TOKEN
 
 for mdl in dat/final_models/*/*r1*jar; do
     echo "--------------------------------------------------------------------------------";
     echo "Trying to upload $mdl ..."
     echo "--------------------------------------------------------------------------------";
-    COMMAND="curl -F \"category=ptptest\" -F \"filecontent=@$mdl\" --header \"Authorization: bearer $TOKEN\" ${MW_URL}api/v1/models/fromFile";
-    echo "Executing command: "$COMMAND
-    RESULT=$($COMMAND)
-    echo "Result from command: "
+    RESULT=`curl -F "category=ptptest" -F "filecontent=@$mdl" --header "Authorization: bearer $TOKEN" ${MW_URL}"/api/v1/models/fromFile"`;
     echo $RESULT
     sleep 0.5
 done;
