59ba5122779842ec2bb5f3ee202ba924f8eecb1f
Samuel Lampa <samuel.lampa@gmail.com>
2018-05-09 15:32:06 +0200

    Restore 2 days run time for full wo-drugbank workflow

--- exp/20180426-wo-drugbank/run-fullwf.sh
+++ exp/20180426-wo-drugbank/run-fullwf.sh
@@ -4,7 +4,7 @@
 #SBATCH -C mem256GB
 #SBATCH -n 20
 #SBATCH -J ptp_fullwf_wo_drugbank
-#SBATCH -t 4:00:00
+#SBATCH -t 2-00:00:00
 #SBATCH --mail-user samuel.lampa@farmbio.uu.se
 #SBATCH --mail-type BEGIN,FAIL,END
 module load java/sun_jdk1.8.0_92
