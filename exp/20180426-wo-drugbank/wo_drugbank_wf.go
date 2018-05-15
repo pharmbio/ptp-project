114162cfa5cd68aa35826cb34f593974d1060dad
Samuel Lampa <samuel.lampa@gmail.com>
2018-05-15 18:30:01 +0200

    Add calibration R-script, workflow component and run script

--- exp/20180426-wo-drugbank/wo_drugbank_wf.go
+++ exp/20180426-wo-drugbank/wo_drugbank_wf.go
@@ -454,6 +454,10 @@ func main() {
 						tsvWrt.Flush()
 					}
 
+					plotCalibrationData := wf.NewProc("plot_calibration_data_"+uniqStrCost, "Rscript bin/plot_calibration.r -i {i:tsv} -o {o:png} -f png")
+					plotCalibrationData.SetPathExtend("tsv", "png", ".png")
+					plotCalibrationData.In("tsv").Connect(extractCalibrationData.Out("tsv"))
+
 					extractCostGammaStats := spc.NewMapToKeys(wf, "extract_cgstats_"+uniqStrCost, func(ip *sp.FileIP) map[string]string {
 						newKeys := map[string]string{}
 						crossValOuts := &[]cpSignCrossValOutput{}
