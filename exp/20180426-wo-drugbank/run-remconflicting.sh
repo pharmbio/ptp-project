74812ae0d85430c1f06c2d63205822a0824bde65
Samuel Lampa <samuel.lampa@gmail.com>
2018-05-22 14:27:25 +0200

    Update remove-conflictin run-script

--- exp/20180426-wo-drugbank/run-remconflicting.sh
+++ exp/20180426-wo-drugbank/run-remconflicting.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 #SBATCH -A snic2017-7-89
 #SBATCH -p core
-#SBATCH -n 4
-#SBATCH -J rem_conflicting
-#SBATCH -t 4:00:00
+#SBATCH -n 8
+#SBATCH -J remove_conflicting
+#SBATCH -t 1:00:00
 #SBATCH --mail-user samuel.lampa@farmbio.uu.se
 #SBATCH --mail-type BEGIN,FAIL,END
 module load java/sun_jdk1.8.0_92
