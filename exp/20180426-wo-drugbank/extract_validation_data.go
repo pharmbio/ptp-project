c74d0fb1d36659e776b4b95b0bd1be4e80b44645
Samuel Lampa <samuel.lampa@gmail.com>
2018-05-30 01:39:30 +0200

    Glob input files with new FileGlobber component

--- exp/20180426-wo-drugbank/extract_validation_data.go
+++ exp/20180426-wo-drugbank/extract_validation_data.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	sp "github.com/scipipe/scipipe"
+	spc "github.com/scipipe/scipipe/components"
 )
 
 func main() {
 	wf := sp.NewWorkflow("extract_valdata", 1)
 
-	extractValidationData := wf.NewProc("extract_validation_data", `cat dat/validate/*/*1000.json \
+	validateFiles := spc.NewFileGlobber(wf, "valstat_files", "dat/validate/*/*1000.json")
+
+	sts := spc.NewStreamToSubStream(wf, "sts")
+	sts.In().Connect(validateFiles.Out())
+
+	extractValidationData := wf.NewProc("extract_validation_data", `cat {i:valjson:r: } \
     | jq -c '[.molecule.activity,.prediction.predictedLabels[0].labels[]]' \
     | tr -d "[" | tr -d "]" | tr -d '"' | \
     awk -F, '{ 
@@ -16,7 +22,8 @@ func main() {
             print d["A"]["",""] "\t" d["A"]["A",""] "\t" d["A"]["N",""] "\t" d["A"]["A","N"]
             print d["N"]["",""] "\t" d["N"]["A",""] "\t" d["N"]["N",""] "\t" d["N"]["A","N"]
 		}' > {o:valstats}`)
-	extractValidationData.SetPathStatic("valstats", "validation_stats.tsv")
+	extractValidationData.SetPathStatic("valstats", "valstats.tsv")
+	extractValidationData.In("valjson").Connect(sts.OutSubStream())
 
 	wf.Run()
 }
