bf34b8f8c8e97f98a577ce660ce9841d8af4b16e
Samuel Lampa <samuel.lampa@gmail.com>
2018-05-30 12:14:24 +0200

    Extract prediction stats per target too

--- exp/20180426-wo-drugbank/extract_validation_data.go
+++ exp/20180426-wo-drugbank/extract_validation_data.go
@@ -3,6 +3,8 @@ package main
 import (
 	sp "github.com/scipipe/scipipe"
 	spc "github.com/scipipe/scipipe/components"
+	"path/filepath"
+	"regexp"
 )
 
 func main() {
@@ -13,17 +15,32 @@ func main() {
 	sts := spc.NewStreamToSubStream(wf, "sts")
 	sts.In().Connect(validateFiles.Out())
 
-	extractValidationData := wf.NewProc("extract_validation_data", `echo -e "Anone\tAA\tAN\tAboth\tNnone\tNA\tNN\tNboth" > {o:valstats} \
-	&& cat {i:valjson:r: } \
-    | jq -c '[.molecule.activity,.prediction.predictedLabels[0].labels[]]' \
+	valDataAll := wf.NewProc("extract_valdata_all", getExtractCmd("{i:valjson:r: }"))
+	valDataAll.SetPathStatic("valstats", "res/validation/valstats.tsv")
+	valDataAll.In("valjson").Connect(sts.OutSubStream())
+
+	valDataPerTarget := wf.NewProc("extract_valdata_pertarget", getExtractCmd("{i:valjson}"))
+	valDataPerTarget.SetPathCustom("valstats", func(t *sp.Task) string {
+		inFile := filepath.Base(t.InPath("valjson"))
+		replacePtn, err := regexp.Compile(`\..*$`)
+		sp.Check(err)
+		gene := replacePtn.ReplaceAllString(inFile, "")
+		return "res/validation/" + gene + "/" + gene + ".valstats.tsv"
+	})
+	valDataPerTarget.In("valjson").Connect(validateFiles.Out())
+
+	wf.Run()
+}
+
+func getExtractCmd(infilePtn string) string {
+	cmd := `echo -e "A->none\tA->A\tA->N\tA->both\tN->none\tN->A\tN->N\tN->both" > {o:valstats} \
+	&& cat ` + infilePtn + ` \
+	| jq -c '[.molecule.activity,.prediction.predictedLabels[0].labels[]]' \
     | tr -d "[" | tr -d "]" | tr -d '"' | \
     awk -F, '{ 
         d[$1][$2,$3]++ } 
         END { 
             print d["A"]["",""] "\t" d["A"]["A",""] "\t" d["A"]["N",""] "\t" d["A"]["A","N"] "\t" d["N"]["",""] "\t" d["N"]["A",""] "\t" d["N"]["N",""] "\t" d["N"]["A","N"]
-		}' >> {o:valstats}`)
-	extractValidationData.SetPathStatic("valstats", "valstats.tsv")
-	extractValidationData.In("valjson").Connect(sts.OutSubStream())
-
-	wf.Run()
+		}' >> {o:valstats}`
+	return cmd
 }
