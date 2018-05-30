611fb1423a1cc3035d6929ce0a2fc3822c0b89c9
Samuel Lampa <samuel.lampa@gmail.com>
2018-05-30 13:29:08 +0200

    Fix script in get_plots and include new plots

--- manuscript_disp/figures/calibration_plots/get_plots.sh
+++ manuscript_disp/figures/calibration_plots/get_plots.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -l
 cp ../../../exp/20180426-wo-drugbank/dat/*/r1/fill/*pdf .
-for f in *.pdf; do g=$(echo $f | sed 's/.r1.*//g'); mv $f $g"_calib.pdf"; done
+for f in *.pdf; do g=$(echo $f | sed 's/\.r1.*//g'); mv $f $g"_calib.pdf"; done
