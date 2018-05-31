b5fdcbafdac1bdfe36fbd11c7c16bcd8b5a1215f
Samuel Lampa <samuel.lampa@gmail.com>
2018-05-31 16:56:10 +0200

    Staffan got a new last name - yeeehoo

---
+++ exp/20180419-fillup-vs-not/create_cleaned_plot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+go run fillup_vs_not.go components.go -geneset smallest1 -graph
+head -n 1 workflow.dot > workflow.cleaned.dot;
+tail -n +2 workflow.dot | head -n -2 | grep -Pv "_r(2|3)" | grep -Pv "_png" | sort | uniq >> workflow.cleaned.dot;
+tail -n 1 workflow.dot >> workflow.cleaned.dot;
+dot -Tpdf workflow.cleaned.dot -o workflow.cleaned.pdf \
+    && exo-open workflow.cleaned.pdf;
