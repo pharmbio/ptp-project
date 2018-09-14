4580d61bdb1a8c82956cf04d58ee7ba629919f03
Samuel Lampa <samuel.lampa@gmail.com>
2018-09-14 21:06:06 +0200

    Works almost, but runs into division by zero

--- exp/20180426-wo-drugbank/extract_validation_data.go
+++ exp/20180426-wo-drugbank/extract_validation_data.go
@@ -16,28 +16,30 @@ func main() {
 	for confIdx, confLevel := range []string{"0p9", "0p8"} { // We use 'p' instead of '.' to avoid confusion in the file name
 		confLevel := confLevel
 
-		// ------------------------------------------------------------------------
-		// Grab input files
-		// ------------------------------------------------------------------------
 		validateFiles := spc.NewFileGlobber(wf, "valstat_files_"+confLevel, "dat/validate/*/*1000.json")
 		sts := spc.NewStreamToSubStream(wf, "sts_"+confLevel)
 		sts.In().From(validateFiles.Out())
 
 		// ------------------------------------------------------------------------
+		// Grab input files
+		// ------------------------------------------------------------------------
+
+		// ------------------------------------------------------------------------
 		// Extract data from JSON
 		// ------------------------------------------------------------------------
-		extractCmdTpl := `echo -e "a_pred_both\ta_pred_a\ta_pred_n\ta_pred_none\tn_pred_both\tn_pred_a\tn_pred_n\tn_pred_none" > {o:valstats} \
-			&& cat %s \
-			| jq -c '[.molecule.activity,.prediction.predictedLabels[%d].labels[]]' \
-			| tr -d "[" | tr -d "]" | tr -d '"' | \
-			awk -F, '{
-				d[$1][$2,$3]++ }
-				END {
-					print d["A"]["A","N"] "\t" d["A"]["A",""] "\t" d["A"]["N",""] "\t" d["A"]["",""] "\t" d["N"]["A","N"] "\t" d["N"]["A",""] "\t" d["N"]["N",""] "\t" d["N"]["",""]
-				}' >> {o:valstats}`
+		extractCmdTpl := `echo -e "orig_lab\tpred_both\tpred_a\tpred_n\tpred_none" > {o:valstats} \
+		&& cat %s \
+		| jq -c '[.molecule.activity,.prediction.predictedLabels[%d].labels[]]' \
+		| tr -d "[" | tr -d "]" | tr -d '"' | \
+		awk -F, '{
+			d[$1][$2,$3]++ }
+			END {
+				print "A" "\t" d["A"]["A","N"] "\t" d["A"]["A",""] "\t" d["A"]["N",""] "\t" d["A"]["",""]
+				print "N" "\t" d["N"]["A","N"] "\t" d["N"]["A",""] "\t" d["N"]["N",""] "\t" d["N"]["",""]
+			}' >> {o:valstats}`
 
 		valDataAll := wf.NewProc("extract_valdata_all_"+confLevel, fmt.Sprintf(extractCmdTpl, "{i:valjson|join: }", confIdx))
-		valDataAll.SetOut("valstats", "res/validation/valstats."+confLevel+".tbl.tsv")
+		valDataAll.SetOut("valstats", "res/validation/valstats."+confLevel+".tsv")
 		valDataAll.In("valjson").From(sts.OutSubStream())
 
 		valDataPerTarget := wf.NewProc("extract_valdata_pertarget_"+confLevel, fmt.Sprintf(extractCmdTpl, "{i:valjson}", confIdx))
@@ -46,9 +48,29 @@ func main() {
 			replacePtn, err := regexp.Compile(`\..*$`)
 			sp.Check(err)
 			gene := replacePtn.ReplaceAllString(inFile, "")
-			return "res/validation/" + gene + "/" + gene + "." + confLevel + ".valstats.tbl.tsv"
+			return "res/validation/" + gene + "/" + gene + "." + confLevel + ".valstats.tsv"
 		})
 		valDataPerTarget.In("valjson").From(validateFiles.Out())
+
+		// ------------------------------------------------------------------------
+		// Plot data
+		// ------------------------------------------------------------------------
+		extractGene := spc.NewMapToTags(wf, "extract_gene_"+confLevel, func(ip *sp.FileIP) map[string]string {
+			ptn, err := regexp.Compile(`\.0p.*\.tsv`)
+			sp.Check(err)
+			gene := strings.ToUpper(ptn.ReplaceAllString(filepath.Base(ip.Path()), ""))
+			return map[string]string{"gene": gene}
+		})
+		extractGene.In().From(valDataPerTarget.Out("valstats"))
+
+		plotValData := wf.NewProc("plot_valdata_"+confLevel, `Rscript bin/plot_valdata.r -i {i:valdata} -o {o:plot} -f pdf -g {k:valdata.gene} -c `+strings.Replace(confLevel, "p", ".", 1))
+		plotValData.SetOut("plot", "{i:valdata}.pdf")
+		plotValData.In("valdata").From(extractGene.Out())
+
+		plotValDataAll := wf.NewProc("plot_valdata_all_"+confLevel, `Rscript bin/plot_valdata.r -i {i:valdata} -o {o:plot} -f pdf -g "all targets" -c `+strings.Replace(confLevel, "p", ".", 1))
+		plotValDataAll.SetOut("plot", "{i:valdata}.pdf")
+		plotValDataAll.In("valdata").From(valDataAll.Out("valstats"))
+	}
 
 	wf.Run()
 }
--- exp/20180426-wo-drugbank/extract_validation_data_table.go
+++ exp/20180426-wo-drugbank/extract_validation_data_table.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"path/filepath"
 	"regexp"
-	"strings"
 
 	sp "github.com/scipipe/scipipe"
 	spc "github.com/scipipe/scipipe/components"
@@ -16,30 +15,40 @@ func main() {
 	for confIdx, confLevel := range []string{"0p9", "0p8"} { // We use 'p' instead of '.' to avoid confusion in the file name
 		confLevel := confLevel
 
+		// ------------------------------------------------------------------------
+		// Grab input files
+		// ------------------------------------------------------------------------
 		validateFiles := spc.NewFileGlobber(wf, "valstat_files_"+confLevel, "dat/validate/*/*1000.json")
 		sts := spc.NewStreamToSubStream(wf, "sts_"+confLevel)
 		sts.In().From(validateFiles.Out())
 
 		// ------------------------------------------------------------------------
-		// Grab input files
-		// ------------------------------------------------------------------------
-
-		// ------------------------------------------------------------------------
 		// Extract data from JSON
 		// ------------------------------------------------------------------------
-		extractCmdTpl := `echo -e "orig_lab\tpred_both\tpred_a\tpred_n\tpred_none" > {o:valstats} \
-		&& cat %s \
-		| jq -c '[.molecule.activity,.prediction.predictedLabels[%d].labels[]]' \
-		| tr -d "[" | tr -d "]" | tr -d '"' | \
-		awk -F, '{
-			d[$1][$2,$3]++ }
-			END {
-				print "A" "\t" d["A"]["A","N"] "\t" d["A"]["A",""] "\t" d["A"]["N",""] "\t" d["A"]["",""]
-				print "N" "\t" d["N"]["A","N"] "\t" d["N"]["A",""] "\t" d["N"]["N",""] "\t" d["N"]["",""]
-			}' >> {o:valstats}`
+		extractCmdTpl := `echo -e "a_pred_both\ta_pred_a\ta_pred_n\ta_pred_none\tn_pred_both\tn_pred_a\tn_pred_n\tn_pred_none\tsens_excl\tsens_incl\tspec_excl\tspec_incl" > {o:valstats} \
+			&& cat %s \
+			| jq -c '[.molecule.activity,.prediction.predictedLabels[%d].labels[]]' \
+			| tr -d "[" | tr -d "]" | tr -d '"' | \
+			awk -F, '{
+				d[$1][$2,$3]++ }
+				END {
+					a_pred_both=d["A"]["A","N"];
+					a_pred_a=d["A"]["A",""];
+					a_pred_n=d["A"]["N",""];
+					a_pred_none=d["A"]["",""];
+					n_pred_both=d["N"]["A","N"];
+					n_pred_a=d["N"]["A",""];
+					n_pred_n=d["N"]["N",""];
+					n_pred_none=d["N"]["",""];
+					sens_excl=a_pred_a/(a_pred_a + a_pred_n);
+					sens_incl=a_pred_a/(a_pred_a + a_pred_n + a_pred_both + a_pred_none);
+					spec_excl=n_pred_n/(n_pred_n + n_pred_a);
+					spec_incl=n_pred_n/(n_pred_n + n_pred_a + n_pred_both + n_pred_none);
+					print a_pred_both "\t" a_pred_a "\t" a_pred_n "\t" a_pred_none "\t" n_pred_both "\t" n_pred_a "\t" n_pred_n "\t" n_pred_none "\t" sens_excl "\t" sens_incl "\t" spec_excl "\t" spec_incl;
+				}' >> {o:valstats}`
 
 		valDataAll := wf.NewProc("extract_valdata_all_"+confLevel, fmt.Sprintf(extractCmdTpl, "{i:valjson|join: }", confIdx))
-		valDataAll.SetOut("valstats", "res/validation/valstats."+confLevel+".tsv")
+		valDataAll.SetOut("valstats", "res/validation/valstats."+confLevel+".tbl.tsv")
 		valDataAll.In("valjson").From(sts.OutSubStream())
 
 		valDataPerTarget := wf.NewProc("extract_valdata_pertarget_"+confLevel, fmt.Sprintf(extractCmdTpl, "{i:valjson}", confIdx))
@@ -48,28 +57,9 @@ func main() {
 			replacePtn, err := regexp.Compile(`\..*$`)
 			sp.Check(err)
 			gene := replacePtn.ReplaceAllString(inFile, "")
-			return "res/validation/" + gene + "/" + gene + "." + confLevel + ".valstats.tsv"
+			return "res/validation/" + gene + "/" + gene + "." + confLevel + ".valstats.tbl.tsv"
 		})
 		valDataPerTarget.In("valjson").From(validateFiles.Out())
-
-		// ------------------------------------------------------------------------
-		// Plot data
-		// ------------------------------------------------------------------------
-		extractGene := spc.NewMapToTags(wf, "extract_gene_"+confLevel, func(ip *sp.FileIP) map[string]string {
-			ptn, err := regexp.Compile(`\.0p.*\.tsv`)
-			sp.Check(err)
-			gene := strings.ToUpper(ptn.ReplaceAllString(filepath.Base(ip.Path()), ""))
-			return map[string]string{"gene": gene}
-		})
-		extractGene.In().From(valDataPerTarget.Out("valstats"))
-
-		plotValData := wf.NewProc("plot_valdata_"+confLevel, `Rscript bin/plot_valdata.r -i {i:valdata} -o {o:plot} -f pdf -g {k:valdata.gene} -c `+strings.Replace(confLevel, "p", ".", 1))
-		plotValData.SetOut("plot", "{i:valdata}.pdf")
-		plotValData.In("valdata").From(extractGene.Out())
-
-		plotValDataAll := wf.NewProc("plot_valdata_all_"+confLevel, `Rscript bin/plot_valdata.r -i {i:valdata} -o {o:plot} -f pdf -g "all targets" -c `+strings.Replace(confLevel, "p", ".", 1))
-		plotValDataAll.SetOut("plot", "{i:valdata}.pdf")
-		plotValDataAll.In("valdata").From(valDataAll.Out("valstats"))
 	}
 
 	wf.Run()
